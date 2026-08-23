1e98ccf1bcdecf4b2eacb7a4a16b6450cd460e69
videolab666 <97349540+videolab666@users.noreply.github.com>
2026-08-23 20:48:21 +0300

    feat: add A/B Event Output source API

---
+++ src/sr-event-output.h
@@ -0,0 +1,13 @@
+/*
+Sports Replay
+Copyright (C) 2026 Systec <systecinformatica@gmail.com> (https://www.systecinformatica.com.ar)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+*/
+
+#pragma once
+
+#define SR_EVENT_OUTPUT_ID "sports_replay_event_output"
