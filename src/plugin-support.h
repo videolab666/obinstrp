4f3a708df0ba520ccb6bbca66bfaab46a5c71348
videolab666 <97349540+videolab666@users.noreply.github.com>
2026-08-28 12:22:07 +0300

    cleanup: replace inherited plugin support header

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,39 +1,26 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
+ * Pitel Instant Replay - small build-generated support API
+ * Copyright (C) 2026 Alexander Pitel
+ *
+ * This OBS plugin component is licensed under the GNU General Public License
+ * version 2 or later. See LICENSE for details.
+ */
 
 #pragma once
+
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
 
 extern const char *PLUGIN_NAME;
 extern const char *PLUGIN_VERSION;
 
 #define PLUGIN_WEBSITE "https://github.com/videolab666/obinstrp"
 
+/* Prefixes one log message with the plugin name before forwarding to libobs. */
 void obs_log(int log_level, const char *format, ...);
-extern void blogva(int log_level, const char *format, va_list args);
 
 #ifdef __cplusplus
 }
